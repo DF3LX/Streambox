c72a7436bb663c6833f3ab4c56081f1f3c139bb4
DO6FP <76111133+DO6FP@users.noreply.github.com>
2021-01-11 16:53:34 +0100

    Update Streamingclient.py
    Final Version of the Browser startup Raspbrry Pi compebilty enchanted.

--- Streamingclient.py
+++ Streamingclient.py
@@ -1,21 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import RPi.GPIO as GPIO
 import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.INPUT)
+GPIO.setup(24, GPIO.OUT)
+
 PATH = "P:\Home\Kirche\Streaming\Streambox\Pogramm\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://rk-solutions-streamc.de/hohenacker/livestream.html")
-print(driver.title)
-xpath = //*[@id="live"]/div[2]/div[1]/div[3]/svg[1]
-search = driver.find_element_by_xpath(xpath)
-search
-print(search)
-driver.maximize_window()
+while True:
+    while GPIO.input(23) == 0:
+    
+    driver.get("https://rk-solutions-streamc.de/hohenacker/livestream.html")
+    driver.maximize_window()
+    driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]").click()
+    
+    while GPIO.input(23) == 1:
+    
+    status = 0
+    while GPIO.input(23) == 0:
+        GPIO.output(24, status)
+        time.sleep(1)
+        if status == 0: status = 1
+        else status = 0
+        
+    driver.quit()
+        
+    while GPIO.input(23) == 1:
 
-
-#driver.find_elements_by_class_name("fp-icon fp-playbtn").click()
-
-time.sleep(5)
-
-driver.quit()
 
